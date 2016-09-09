469b3fb556798dea1201d4018824bbd8ea5a08db
Keith Owens <kowens@pekininsurance.com>
2016-09-09 15:24:14 -0500

    Reorganize files.

---
+++ Server.Test/ServerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Server.Test
+{
+    [TestClass]
+    public class ServerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
