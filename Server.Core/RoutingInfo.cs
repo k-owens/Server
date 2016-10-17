5fd273d252b3c5880b08541de3825695d5faaa90
Keith Owens <kowens@pekininsurance.com>
2016-10-17 09:12:35 -0500

    Add ability to use router to create paths for request handlers.

---
+++ Server.Core/RoutingInfo.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Core
+{
+    public class RoutingInfo
+    {
+        public HttpMethod Method { get; }
+        public string Uri { get; }
+        public IRequestHandler HandlerToRun { get; }
+
+        public RoutingInfo(HttpMethod method, string uri, IRequestHandler handlerToRun)
+        {
+            Method = method;
+            Uri = uri;
+            HandlerToRun = handlerToRun;
+        }
+    }
+}
